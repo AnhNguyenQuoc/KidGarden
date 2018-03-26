3d19110039e279c89b6c551eea37269c9a0e172d
Anh Nguyen <nguyenquocanh121296@gmail.com>
2018-03-26 15:16:36 +0700

    Add some routers for user

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#index'
-  devise_scope do
-    get '/signup' => 'devise/registrantions#new'
-    get '/edit' => 'devise/registrantions#edit'
+  devise_scope :user do
+    get '/sign_up' => 'devise/registrations#new'
+    get '/edit' => 'devise/registrations#edit'
     get '/login'  => 'devise/sessions#create'
     get '/logout' => 'devise/sessions#destroy'
     get '/forget_password' => 'devise/passwords#new'
