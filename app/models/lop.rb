601a95fae5c1386f39e6548575c0831f74bbe0e6
Anh Nguyen <nguyenquocanh121296@gmail.com>
2018-05-01 13:54:12 +0700

    Change Model Lop

--- app/models/lop.rb
+++ app/models/lop.rb
@@ -3,7 +3,7 @@ class Lop < ApplicationRecord
   before_save {self.lophoc = lophoc.downcase}
 
 
-  VALID_LOPHOC = /(chồi|mầm|lá)?[0-9]+/
+  VALID_LOPHOC = /(chồi|mầm|lá)? [0-9]+/
 
   validates :khoihoc, presence: true, length: {minimum: 6}
   validates :lophoc, presence: true, length: {minimum: 6}, format: {with: VALID_LOPHOC}
